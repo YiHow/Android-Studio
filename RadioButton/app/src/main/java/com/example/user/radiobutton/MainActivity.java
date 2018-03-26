b63cddada1cdec6bb78ae4ea51106e5ec730a1dd
YiHow <ivor0825@gmail.com>
2018-03-26 16:00:26 +0800

    123

---
+++ RadioButton/app/src/main/java/com/example/user/radiobutton/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.user.radiobutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RadioGroup rg = findViewById(R.id.rg);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId){
+                    case R.id.rbtn1:
+
+
+                }
+            }
+        });
+    }
+}
